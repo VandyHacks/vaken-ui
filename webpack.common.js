c891131f9cb53e319e0fc5a80b9255ee78669694
Matthew Leon <matthew.leon@vanderbilt.edu>
2019-01-02 17:39:11 -0500

    use tabs in webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -3,42 +3,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    context: __dirname, // to automagically find tsconfig.json
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
-    },
-    module: {
-        rules: [{
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'thread-loader',
-                    options: {
-                        workers: require('os').cpus().length - 1, // Leave 1 worker for fork-ts-checker
-                        poolTimeout: Infinity // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
-                    },
-                },
-                {
-                    loader: 'babel-loader',
-                    /* loader: 'ts-loader', // Alternative
+	context: __dirname, // to automagically find tsconfig.json
+	entry: './src/index',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'app.bundle.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				// Include ts, tsx, js, and jsx files.
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'thread-loader',
+						options: {
+							workers: require('os').cpus().length - 1, // Leave 1 worker for fork-ts-checker
+							poolTimeout: Infinity, // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+						},
+					},
+					{
+						loader: 'babel-loader',
+						/* loader: 'ts-loader', // Alternative
                     options: {
                         happyPackMode: true // use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
                     } */
-                }
-            ],
-        }],
-    },
-    plugins: [
-        new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }), // Check types asynchronously
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }), // For index.html entry point
-    ]
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }), // Check types asynchronously
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}), // For index.html entry point
+	],
 };
